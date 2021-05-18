b565b08a83f0c654ec28720281a5e838e33a589d
Elias Abreu do Couto <73173398+helyascouto@users.noreply.github.com>
2021-05-18 10:57:30 -0300

    up

---
+++ Aceleração Global Dev 7 Avanade/Substituição em Vetor/Substituição_em_Vetor.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Substituição_em_Vetor
+{
+    class Substituição_em_Vetor
+    {
+        static void Main(string[] args)
+        {
+            int[] X = new int[10];
+            int y = 0;
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($" Digite um valor para X[{i}] ");
+                X[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            for (int i = 0; i < 10; i++)
+            {
+                if (X[i] >= 0 && X[i] != null)
+                {
+                    Console.WriteLine($" X[{i}] = {X[i]}");
+                }
+                else
+                {
+                    X[i] = 1;
+                    Console.WriteLine($" X[{i}] = {X[i]}");
+                }
+
+            }
+        }
+    }
+}
---
+++ Aceleração Global Dev 7 Avanade/Tipo de combustivel/Tipo_de_combustivel.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tipo_de_combustivel_1
+{
+    class Tipo_de_combustivel
+    {
+        static void Main(string[] args)
+        {
+            int escolha = 0;
+            int gasolina = 0;
+            int alcool = 0;
+            int diesel = 0;
+            do
+            {
+
+                Console.WriteLine(" 1-Álcool | 2-Gasolina | 3-Diesel |4-Sair");
+                escolha = Convert.ToInt32(Console.ReadLine());
+
+                switch (escolha)
+                {
+                    case 1:
+                        alcool += +1;
+                        break;
+
+                    case 2:
+                        gasolina += +1;
+                        break;
+
+                    case 3:
+                        diesel += +1;
+                        break;
+
+                    case 4:
+                        Console.WriteLine($" Obrigado por utilizar nosso sistema! \n Alcool: {alcool} Gasolina: {gasolina} Diesel: {diesel})");
+
+                        break;
+
+                    default:
+                        Console.WriteLine("Código inválido (fora da faixa de 1 a 4)");
+                        break;
+                }
+            }
+
+            while (escolha != 4);
+
+        }
+    }
+
+}
